72904c2418aaa8afc802bde140ee180f227cbc34
Christofer Hedbrandh <christofer.hedbrandh@skola.sala.se>
2024-03-12 12:50:05 +0100

    Add project files.

---
+++ Medelberäknaren - Utan Klasser/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Medelberäknaren___Utan_Klasser
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            double numberOfValues = 0;
+
+            // en evighetsloop
+            while (true)
+            {
+                // ta input från användaren
+                Console.Write("Skriv in ett heltal: ");
+                int value = int.Parse(Console.ReadLine());
+
+                // uppdatera summan och antal input från användaren
+                sum = sum + value;
+                numberOfValues++;
+
+                // skriv ut det nya medelvärdet
+                Console.WriteLine("Medelvärdet är " + sum / numberOfValues);
+            }
+        }
+    }
+}
